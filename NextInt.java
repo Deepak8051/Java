682aad136be2d627c8d26f23a362ef77ebbb049f
Deepak8051 <Dsharma813101@gmail.com>
2023-07-01 23:41:26 +0530

    Commit

---
+++ NextInt.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+class nextInt {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter an integer: ");
+        int data1 = input.nextInt();
+        System.out.println("Using nextInt(): " + data1);
+        input.close();
+    }
+}

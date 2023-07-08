22917b0f1e9bbbcdb0c6c2e959edafea559251c5
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-07-08 23:43:21 +0530

    Add files via upload

---
+++ Str7.java
@@ -0,0 +1,11 @@
+public class Str7 {
+   
+        public static void main(String[] args) {
+          String str1 = "C++ Programming";
+       System.out.println(str1.replace("C++", "Java"));  // Java Programming
+       System.out.println("aa bb aa zz".replace("aa", "zz")); // zz bb zz zz
+      System.out.println("Java".replace("C++", "C")); // Java
+        }
+      }
+    
+

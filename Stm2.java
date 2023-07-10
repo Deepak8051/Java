dbe215cb749c7ba07305950e8b3b6107f61808c7
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-07-10 22:49:06 +0530

    If else statement

---
+++ Stm2.java
@@ -0,0 +1,14 @@
+public class Stm2 {
+    
+        public static void main(String[] args) {
+          // create a string variable
+          String language = "Java";
+      
+          // if statement
+          if (language == "Java") {
+            System.out.println("Best Programming Language");
+          }
+        }
+      }
+    
+

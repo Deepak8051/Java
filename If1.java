e0774960d6da00e831e9424c5292a3b357a94626
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-07-19 23:54:09 +0530

    Add files via upload

---
+++ If1.java
@@ -0,0 +1,15 @@
+// Java program to illustrate If statement without curly block
+import java.util.*;
+
+class IfDemo {
+	public static void main(String args[])
+	{
+		int i = 10;
+
+		if (i < 15)
+			System.out.println("Inside If block"); 
+			System.out.println("10 is less than 15"); 
+		
+		System.out.println("I am Not in if");
+	}
+}

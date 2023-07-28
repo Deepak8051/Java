a8378fe34084eb2e5e8c080afba0c781150b8bd9
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-07-28 22:46:11 +0530

    Add files via upload

---
+++ As.java
@@ -0,0 +1,19 @@
+public class As {
+    
+     
+        private String name;
+    }
+    
+    public class Maa {
+        public static void main(String[] main){
+    
+            // create an object of Data
+            Maa d = new Maa();
+    
+            // access private variable and field from another class
+            d.name = "Programiz";
+        }
+    }
+    
+    
+

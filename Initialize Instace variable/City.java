089a4521006a21817b169b479ed0cb402dfc70e7
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-08-30 23:05:23 +0530

    Initialize instance variable

---
+++ Initialize Instace variable/City.java
@@ -0,0 +1,9 @@
+class City {
+   String Name;
+   public static void main(String[]args){
+   City c = new City();
+  c.Name="Bangalore";
+System.out.println(c.Name);
+
+}
+}
---
+++ Initialize Instace variable/Person.java
@@ -0,0 +1,8 @@
+ class Person {
+   int age = 25;
+   public static void main(String[]args) {
+  Person p1 = new Person();
+   System.out.println(p1.age);
+   p1.age=24;
+}
+}

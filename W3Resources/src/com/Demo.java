36cd4c5a979b0eb6c02b38aa94a7401ee80c2545
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-11-05 23:36:47 +0530

    Add files via upload

---
+++ W3Resources/src/com/Demo.java
@@ -0,0 +1,12 @@
+//Write a Java program to print 'Hello' on screen and your name on a separate line.
+package com;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("hello \n Deepak");
+		
+
+	}
+
+}
---
+++ W3Resources/src/com/Demo1.java
@@ -0,0 +1,14 @@
+package com;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println(20+20);
+		System.out.println(50/3);
+		System.out.println(-5+8*6);
+		System.out.println((55+9)%9);
+		System.out.println(20+-3*5/8);
+		System.out.println(5+15/3*2-8%3);
+	}
+
+}

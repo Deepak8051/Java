655056561ec8a30665be6bcaaf65a8dbfa815988
Deepak kumar Sharma <101473013+Deepak8051@users.noreply.github.com>
2023-11-07 23:32:44 +0530

    Add files via upload

---
+++ class_method/Main.java
@@ -0,0 +1,18 @@
+package class_method;
+
+public class Main {
+	 
+		  public static void main(String[] args) {
+			  Student s = new Student();
+			  s.name="Deepak";
+			  s.age=23;
+			  s.roll=123;
+			  s.Address="Bnagalore";
+			  s.USN="1bi21mc019";
+			  s.detail();
+			
+		}
+	  }
+	  
+
+
---
+++ class_method/Student.java
@@ -0,0 +1,16 @@
+package class_method;
+
+public class Student {
+	String name;
+	String USN;
+	String Address;
+	int roll;
+	int age;
+
+  public void detail() {
+	  System.out.print("name: " +name+ " usn:   "+USN+ " address: "+Address+ " rollno: "+roll+ "age:  "+ age);
+	 
+	
+
+}
+}
